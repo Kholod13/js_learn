ef31a1a2136ef765b831a5508c79f56865b25e30
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2024-02-04 21:50:31 +0100

    finish task 1 objects

--- 03_objects/01_task/main.js
+++ 03_objects/01_task/main.js
@@ -1,8 +1,47 @@
 var time = {
-   hours: 21,
-   minutes: 23,
-   seconds: 44
+   hours: 23,
+   minutes: 59,
+   seconds: 59,
+
+   ShowTime: function(){
+      let h = this.hours;
+      let m = this.minutes;
+      let s = this.seconds;
+
+      if(this.hours == 0){
+         h = "00";
+      }
+      if(this.minutes == 0){
+         m = "00";
+      }
+      if(this.seconds == 0){
+         s = "00";
+      }
+
+      console.log(`Time: ${h}:${m}:${s}`);
+   },
+   TimePlus: function(){
+      this.seconds++;
+      if(this.seconds == 60){
+         this.seconds = 0;
+
+         this.minutes++;
+         if(this.minutes == 60){
+            this.minutes = 0;
+
+            this.hours++;
+            if(this.hours == 24){
+               this.hours = 0;
+            }
+         }
+      }
+   },
+   TimeMinus: function(){
+      time.seconds--;
+   },
 }
+//using functions
+/*
 function ShowTime(time){
    console.log(`Time: ${time.hours}:${time.minutes}:${time.seconds}`);
 }
@@ -17,3 +56,11 @@ TimeMinus(time);
 ShowTime(time);
 TimePlus(time);
 ShowTime(time);
+*/
+
+//using methods
+time.ShowTime();
+time.TimePlus();
+time.ShowTime();
+time.TimePlus();
+time.ShowTime();
